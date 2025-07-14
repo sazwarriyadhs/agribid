9d5e32035db26f156e7633e833b4ae50b79f32cc
Kunyit Asem <kunyitasemmanispedas@gmail.com>
2025-07-14 01:57:20 +0000

    commit

--- src/components/hero-slider.tsx
+++ src/components/hero-slider.tsx
@@ -16,19 +16,19 @@ import { useI18n } from '@/context/i18n';
 
 const heroSlides = [
   {
-    src: 'https://placehold.co/1920x1080.png',
+    src: '/images/hero1.png',
     aiHint: 'farmer field sunrise',
     titleKey: 'hero_title',
     subtitleKey: 'hero_subtitle',
   },
   {
-    src: 'https://placehold.co/1920x1080.png',
+    src: '/images/hero2.png',
     aiHint: 'agricultural trade market',
     titleKey: 'hero_title_2',
     subtitleKey: 'hero_subtitle_2',
   },
   {
-    src: 'https://placehold.co/1920x1080.png',
+    src: '/images/hero3.png',
     aiHint: 'fresh produce export logistics',
     titleKey: 'hero_title_3',
     subtitleKey: 'hero_subtitle_3',
